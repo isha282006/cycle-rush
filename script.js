d8c6e6b25c639d6feec84d0127244fece2f78b5d
Isha Vishnoi <ishavishnoi28@gmail.com>
2026-01-11 15:40:47 +0530

    script.js

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ document.getElementById("bookingForm").addEventListener("submit", function(e) {
   e.preventDefault();
   document.getElementById("msg").innerText = "✅ Booking Successful!";
 });
+// Load saved reviews when page opens
+window.onload = function(){
+  let saved = JSON.parse(localStorage.getItem("reviews"));
+  if(saved){
+    saved.forEach(r => showReview(r.name, r.text));
+  }
+};
+
 function addReview(){
   let name = document.getElementById("reviewName").value;
   let text = document.getElementById("reviewText").value;
 
   if(name=="" || text=="") return;
 
-  let review = document.createElement("div");
-  review.className="review-item";
-  review.innerHTML = `<b>${name}</b><p>${text}</p>`;
+  let reviews = JSON.parse(localStorage.getItem("reviews")) || [];
+  reviews.push({name, text});
+  localStorage.setItem("reviews", JSON.stringify(reviews));
 
-  document.getElementById("reviewList").prepend(review);
+  showReview(name, text);
 
   document.getElementById("reviewName").value="";
   document.getElementById("reviewText").value="";
+}
+
+function showReview(name, text){
+  let review = document.createElement("div");
+  review.className="review-item";
+  review.innerHTML = `<b>${name}</b><p>${text}</p>`;
+  document.getElementById("reviewList").prepend(review);
 }
 
 
