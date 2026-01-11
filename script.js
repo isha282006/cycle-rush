3d4bc89c11987cdf090aa26104215fc1bc0757e1
Isha Vishnoi <ishavishnoi28@gmail.com>
2026-01-11 15:37:23 +0530

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ document.getElementById("bookingForm").addEventListener("submit", function(e) {
   e.preventDefault();
   document.getElementById("msg").innerText = "✅ Booking Successful!";
 });
+function addReview(){
+  let name = document.getElementById("reviewName").value;
+  let text = document.getElementById("reviewText").value;
+
+  if(name=="" || text=="") return;
+
+  let review = document.createElement("div");
+  review.className="review-item";
+  review.innerHTML = `<b>${name}</b><p>${text}</p>`;
+
+  document.getElementById("reviewList").prepend(review);
+
+  document.getElementById("reviewName").value="";
+  document.getElementById("reviewText").value="";
+}
+
+
